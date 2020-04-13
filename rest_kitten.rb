83394b1b9c874f964fa6a9c1fa31daf664197301
jungseob1kim <jungseob1.kim@gmail.com>
2020-04-13 20:05:23 +1000

    initial commit

---
+++ rest_kitten.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+require 'sinatra'
+
+get '/kittens' do
+    width = rand(1..20) * 100
+    height = rand(1..20) * 100
+    array_kittens = []
+    50.times do |i|
+        kitten = {}
+        kitten[:width] = width
+        kitten[:height] = height
+        kitten[:img_path] = "http://placekitten.com/#{width}/#{height}"
+        array_kittens.push(kitten)
+    end
+    array_kitten.to_json
+end
